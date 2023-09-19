0763189816406c61c3a145052a3aafb77e7aac29
antrioe <antrioe@gmali.com>
2023-09-19 17:00:42 +0800

    ci(deps): ci

--- api/_routes.ts
+++ api/_routes.ts
@@ -2,9 +2,12 @@ import { Router } from 'express'
 import info from './info'
 import login from './login'
 import status from './status'
+import track from './track'
 
 const router = Router()
 router.get('/info', info)
 router.post('/login', login)
 router.get('/status', status)
+router.post('/track', track)
+
 export default router
--- src/views/index/analysis.vue
+++ src/views/index/analysis.vue
@@ -7,13 +7,21 @@ onDeactivated(() => {
 
 <template>
   <default-card h-209 title="分析页">
-    <div wfull flex-center rounded-xl bg="white dark:dark">
+    <div wfull h-full flex-col-center gap-10 rounded-xl bg="white dark:dark">
       <h1 text-3xl>
         {{ t('welcome') }}
       </h1>
-      <n-button type="primary">
-        Primary
-      </n-button>
+      <div class="flex-center gap-1">
+        <NButton v-use-track:exposure>
+          ExposureTrack
+        </NButton>
+        <NButton v-use-track:click>
+          ClickTrack
+        </NButton>
+        <NButton v-use-track:long-press>
+          LongPressTrack
+        </NButton>
+      </div>
     </div>
   </default-card>
 </template>
